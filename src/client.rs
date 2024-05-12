310ee6ac4d10cd3692c269ba118fcff645e485dc
stromokocur5 <gabrielkohut3@proton.me>
2024-05-12 21:33:20 +0200

    :p

--- src/client.rs
+++ src/client.rs
@@ -27,7 +27,7 @@ async fn install() -> Result<()> {
         .stderr(Stdio::null())
         .spawn()?;
     sh.wait()?;
-    let required_packages = &["mpv", "alsa-tools"];
+    let required_packages = &["mpv", "alsa-tools", "feh"];
     for i in required_packages {
         upt_install(i).await?;
     }
@@ -68,17 +68,126 @@ async fn always_on_audio() -> Result<()> {
     Ok(())
 }
 async fn record_audio() -> Result<()> {
+    use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+    use cpal::Sample;
+    use std::fmt::Debug;
+
+    let host = cpal::default_host();
+
+    for device in host.input_devices()? {
+        match device {
+            x if device.name()?.starts_with("sysdefault") => {
+                let device = x;
+
+                let config = device
+                    .default_input_config()
+                    .expect("Failed to get default input config");
+                println!("Default input config: {:?}", config);
+
+                let err_fn = move |err| {
+                    eprintln!("an error occurred on stream: {}", err);
+                };
+
+                let stream = match config.sample_format() {
+                    cpal::SampleFormat::I8 => device.build_input_stream(
+                        &config.into(),
+                        move |data, _: &_| write_input_data::<i8>(data),
+                        err_fn,
+                        None,
+                    )?,
+                    cpal::SampleFormat::I16 => device.build_input_stream(
+                        &config.into(),
+                        move |data, _: &_| write_input_data::<i16>(data),
+                        err_fn,
+                        None,
+                    )?,
+                    cpal::SampleFormat::I32 => device.build_input_stream(
+                        &config.into(),
+                        move |data, _: &_| write_input_data::<i32>(data),
+                        err_fn,
+                        None,
+                    )?,
+                    cpal::SampleFormat::F32 => device.build_input_stream(
+                        &config.into(),
+                        move |data, _: &_| write_input_data::<f32>(data),
+                        err_fn,
+                        None,
+                    )?,
+                    sample_format => {
+                        return Err(anyhow::Error::msg(format!(
+                            "Unsupported sample format '{sample_format}'"
+                        )))
+                    }
+                };
+
+                stream.play().unwrap();
+                tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+                stream.pause().unwrap();
+            }
+            x => {}
+        }
+    }
+    fn write_input_data<T: Sample + Debug>(input: &[T]) {
+        println!("{:?}", input);
+    }
+
     Ok(())
 }
-async fn record_video() -> Result<()> {
-    use tokio_linux_video::Device;
-    let mut devs = Device::list().await?;
-    while let Some(path) = devs.fetch_next().await? {
-        let dev = Device::open(&path).await?;
+async fn screen_capture() -> Result<()> {
+    use xcap::Monitor;
 
-        let caps = dev.capabilities().await?;
-
-        println!("path: {}, {caps}", path.display());
+    fn normalized(filename: &str) -> String {
+        filename
+            .replace("|", "")
+            .replace("\\", "")
+            .replace(":", "")
+            .replace("/", "")
     }
+
+    let monitors = Monitor::all().unwrap();
+
+    for monitor in monitors {
+        let image = monitor.capture_image().unwrap();
+
+        let date = chrono::Local::now()
+            .format("%d_%m_%Y__%H_%M_%S")
+            .to_string();
+        image
+            .save(format!(
+                "monitor-{}__{date}.png",
+                normalized(monitor.name()),
+            ))
+            .unwrap();
+    }
+
+    Ok(())
+}
+async fn record_webcam() -> Result<()> {
+    use tokio_linux_video::{types::*, Device};
+    let dev = Device::open("/dev/video0").await?;
+
+    let mut fmt = dev.format(BufferType::VideoCapture).await?;
+    println!("{fmt}");
+
+    let stream = dev.stream::<In, Mmap>(ContentType::Video, 4)?;
+
+    let mut i = 0;
+    let start = tokio::time::Instant::now();
+    while let Ok(buffer) = stream.next().await {
+        let buffer = buffer.lock();
+        println!("#{buffer}");
+
+        let _data: &[u8] = buffer.as_ref();
+        tokio::fs::write(format!("{i}.jpg"), _data).await?;
+        if start.elapsed() >= tokio::time::Duration::from_secs(5) {
+            break;
+        }
+        i += 1;
+    }
+
+    Ok(())
+}
+
+async fn set_wallpaper() -> Result<()> {
     Ok(())
 }
